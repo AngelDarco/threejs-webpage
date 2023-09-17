be71c3a5cc91341c0f55d37cda7854c6c3833209
Angel Darco <angel92-eme@hotmail.com>
2023-09-17 11:37:55 -0500

    :sparkles: progress loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Model from "./scenes/Model";
+import { Suspense } from "react";
+import Loader from "./components/loader/Loader";
 function App() {
   return (
     <Canvas shadows camera={{ fov: 50 }}>
       <ambientLight intensity={1.8} />
-      <Model rotation={[0, 10, 0]} position={[200, -220, -850]} />
+      <Suspense fallback={<Loader />}>
+        <Model rotation={[0, 10, 0]} position={[200, -220, -1000]} />
+      </Suspense>
     </Canvas>
   );
 }
