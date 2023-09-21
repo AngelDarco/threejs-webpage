b8c215394f3200d92b060ac594d8a9b3446399ab
Angel Darco <angel92-eme@hotmail.com>
2023-09-21 09:49:18 -0500

    :sparkles: switch between animations added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import Model from "./scenes/Model";
-import { Suspense } from "react";
+// import Model from "./scenes/girl-reverence/Model";
+// import Bird from "./scenes/bird/Bird";
+import Rex from "./scenes/rex/Rex";
+import { Suspense, useRef, useState } from "react";
 import Loader from "./components/loader/Loader";
 function App() {
+  const [animation, setAnimation] = useState(4);
+  const ref = useRef(animation);
+
+  const handlerAnimations = () => {
+    ref.current++;
+    if (ref.current > 4) ref.current = 0;
+
+    setAnimation(ref.current);
+  };
+
   return (
-    <Canvas shadows camera={{ fov: 50 }}>
-      <ambientLight intensity={1.8} />
-      <Suspense fallback={<Loader />}>
-        <Model rotation={[0, 10, 0]} position={[200, -220, -1000]} />
-      </Suspense>
-    </Canvas>
+    <>
+      {/* <div className="bg-transparent flex w-full h-screen items-center justify-around [&>*]:bg-inherit [&>*]:w-[200px] absolute top-0 left-0 z-20 ">
+        <h1 className="text-4xl text-center !w-[300px] text-green-500">
+          Always the best in the field
+        </h1>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor debitis
+          aut voluptatem perferendis dolores accusantium iusto explicabo, vitae
+          mollitia corporis sint vel sunt voluptas perspiciatis rem labore
+          recusandae repellat. Quaerat!
+        </span>
+      </div> */}
+      <Canvas shadows>
+        <ambientLight intensity={1.8} />
+        <pointLight position={[1, 0, 2]} intensity={1} />
+
+        <Suspense fallback={<Loader />}>
+          <Rex
+            position={[-1, -1.5, 0]}
+            scale={2}
+            handlerAnimations={handlerAnimations}
+            anime={animation}
+          />
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
 
